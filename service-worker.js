b95e74d47bd6f3189178717d8b4b5152f35bcb4a
chenzongjian-ship-it <2790569814@qq.com>
2026-06-01 00:38:39 +0800

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-const CACHE_NAME='aurevia-mobile-v66';
-self.addEventListener('install', e=>self.skipWaiting());
-self.addEventListener('activate', e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event=>{
-  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
-});
